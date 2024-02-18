80b06ff3de2ff1e1aff8a51b7017f60d9a76a2bd
ergossteam <ergossteam@gmail.com>
2024-02-18 14:58:59 +0600

    add: url_shortener repo implementation

--- internal/app/url_shortener/repository.go
+++ internal/app/url_shortener/repository.go
@@ -1,4 +1,29 @@
 package urlshortener
 
-type UrlShortenerRepository struct {
+import (
+	"context"
+
+	"github.com/Ergos1/url-shortener.git/infrastructure/db/psql"
+)
+
+type UrlShortenerPsqlRepository struct {
+	db psql.PGX
+}
+
+func NewUrlShortenerPsqlRepository(db psql.PGX) *UrlShortenerPsqlRepository {
+	return &UrlShortenerPsqlRepository{
+		db: db,
+	}
+}
+
+func (r *UrlShortenerPsqlRepository) Create(ctx context.Context, url Url) (int64, error) {
+	var id int64
+	err := r.db.Create(ctx, &id, "INSERT INTO urls(short_url, original_url) VALUES($1, $2) RETURNING id", url.ShortUrl, url.OriginalUrl)
+	return id, err
+}
+
+func (r *UrlShortenerPsqlRepository) GetByOriginalUrl(ctx context.Context, originalUrl string) (*UrlRow, error) {
+	var url UrlRow
+	err := r.db.Get(ctx, &url, "SELECT * FROM urls WHERE original_url = $1", originalUrl)
+	return &url, err
 }
--- internal/app/url_shortener/service.go
+++ internal/app/url_shortener/service.go
@@ -4,7 +4,7 @@ import "context"
 
 type UrlRepository interface {
 	Create(ctx context.Context, url Url) (int64, error)
-	GetByOriginalUrl(ctx context.Context, originalUrl string) (string, error)
+	GetByOriginalUrl(ctx context.Context, originalUrl string) (*UrlRow, error)
 }
 
 type UrlShortenerService struct {
